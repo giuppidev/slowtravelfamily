5d99d0fb735f822d37f30167d2839c1543b46392
Giuseppe Funicello <g.funicello@gmail.com>
2023-05-23 08:28:48 +0100

    fix: removed console.log

--- utils/cloudinary.ts
+++ utils/cloudinary.ts
@@ -1,16 +1,6 @@
 import path from "path";
 
 export function getCloudinaryPath(filename: string): string {
-  console.log(
-    new URL(
-      path.join(
-        "w_1000,h_1000,c_limit",
-        process.env.NEXT_PUBLIC_CLOUDINARY_DOMAIN_NAME || "",
-        filename
-      ),
-      process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_BASE_URL
-    ).href
-  );
   return new URL(
     path.join(
       "w_1000,h_1000,c_limit",
